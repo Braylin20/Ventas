f3424faed01db536f5053fddd89dd4cdd251ba5f
Braylin <braylinvasquez20@gmail.com>
2024-10-16 16:31:41 -0400

    Agregadno metodo para editar

--- app/src/main/java/ucne/edu/ventas/local/remote/VentasApi.kt
+++ app/src/main/java/ucne/edu/ventas/local/remote/VentasApi.kt
@@ -4,6 +4,8 @@ import kotlinx.coroutines.flow.Flow
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 import ucne.edu.ventas.local.remote.dto.VentaDto
 
 interface VentasApi {
@@ -11,4 +13,6 @@ interface VentasApi {
     suspend fun getVentas(): List<VentaDto>
     @POST("api/Ventas")
     suspend fun addVentas(@Body ventasDto: VentaDto): VentaDto
+    @PUT("api/Ventas/{id}")
+    suspend fun updateVenta(@Path("id") id: Int, @Body ventaDto: VentaDto): VentaDto
 }
