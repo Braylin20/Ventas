24dd4663a036fd7de646e55c1dbec194f2157a95
Braylin <braylinvasquez20@gmail.com>
2024-10-15 22:29:36 -0400

    Creando VentasApi

--- app/src/main/java/ucne/edu/ventas/local/remote/VentasApi.kt
+++ app/src/main/java/ucne/edu/ventas/local/remote/VentasApi.kt
@@ -1,14 +1,14 @@
-package ucne.edu.ventas.local.data.remote.api
+package ucne.edu.ventas.local.remote
 
 import kotlinx.coroutines.flow.Flow
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
-import ucne.edu.ventas.local.data.remote.dto.VentaDto
+import ucne.edu.ventas.local.remote.dto.VentaDto
 
 interface VentasApi {
     @GET("api/Ventas")
-    suspend fun getVentas(): Flow<List<VentaDto>>
+    suspend fun getVentas(): List<VentaDto>
     @POST("api/Ventas")
     suspend fun addVentas(@Body ventasDto: VentaDto): VentaDto
 }
